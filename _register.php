9cd38b2f1bf4d68d27e31d26bf99514b38752ca7
Olha Livitchuk <olha.livitchuk@spryker.com>
2024-08-14 14:00:47 +0200

    FRW-8618 Adjustments

--- _register.php
+++ _register.php
@@ -7,8 +7,6 @@ use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
 use Spryker\Shared\Opentelemetry\Request\RequestProcessor;
 
 if (extension_loaded('opentelemetry') === false) {
-    trigger_error('The opentelemetry extension must be loaded in order to autoload the OpenTelemetry Spryker Framework auto-instrumentation', E_USER_WARNING);
-
     return;
 }
 
