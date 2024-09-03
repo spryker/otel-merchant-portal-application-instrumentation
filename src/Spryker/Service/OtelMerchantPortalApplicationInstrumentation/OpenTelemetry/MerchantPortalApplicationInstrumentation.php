9d91c1ffd51c404767a81d41a70a50b31195ca7f
Serhii Chepela <serhii.chepela@spryker.com>
2024-09-03 10:38:35 +0300

    FRW-8618 updated code prior to release

--- src/Spryker/Service/OtelMerchantPortalApplicationInstrumentation/OpenTelemetry/MerchantPortalApplicationInstrumentation.php
+++ src/Spryker/Service/OtelMerchantPortalApplicationInstrumentation/OpenTelemetry/MerchantPortalApplicationInstrumentation.php
@@ -10,15 +10,15 @@ namespace Spryker\Service\OtelMerchantPortalApplicationInstrumentation\OpenTelem
 use Exception;
 use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
 use OpenTelemetry\API\Trace\Span;
-use OpenTelemetry\API\Trace\SpanInterface;
 use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\Context\ContextStorageScopeInterface;
 use OpenTelemetry\SemConv\TraceAttributes;
-use Spryker\Shared\MerchantPortalApplication\MerchantPortalApplication;
 use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
 use Spryker\Shared\Opentelemetry\Request\RequestProcessor;
+use Spryker\Zed\MerchantPortalApplication\Communication\Bootstrap\MerchantPortalBootstrap;
+use Spryker\Zed\Opentelemetry\Business\Generator\SpanFilter\SamplerSpanFilter;
 use Symfony\Component\HttpFoundation\Request;
 use Throwable;
 use function OpenTelemetry\Instrumentation\hook;
@@ -60,15 +60,16 @@ class MerchantPortalApplicationInstrumentation
      */
     public static function register(): void
     {
-        $instrumentation = new CachedInstrumentation();
         $request = new RequestProcessor();
 
         // phpcs:disable
         hook(
-            class: MerchantPortalApplication::class,
+            class: MerchantPortalBootstrap::class,
             function: static::METHOD_NAME,
-            pre: static function ($instance, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation, $request): void {
-                if ($instrumentation::getCachedInstrumentation() === null || $request->getRequest() === null) {
+            pre: static function ($instance, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($request): void {
+                putenv('OTEL_SERVICE_NAME=MERCHANT_PORTAL');
+                $instrumentation = CachedInstrumentation::getCachedInstrumentation();
+                if ($instrumentation === null || $request->getRequest() === null) {
                     return;
                 }
 
@@ -79,7 +80,7 @@ class MerchantPortalApplicationInstrumentation
                 $input = [static::MERCHANT_PORTAL_TRACE_ID => OTEL_MERCHANT_PORTAL_TRACE_ID];
                 TraceContextPropagator::getInstance()->inject($input);
 
-                $span = $instrumentation::getCachedInstrumentation()
+                $span = $instrumentation
                     ->tracer()
                     ->spanBuilder(static::formatSpanName($request->getRequest()))
                     ->setSpanKind(SpanKind::KIND_SERVER)
@@ -100,7 +101,8 @@ class MerchantPortalApplicationInstrumentation
                     return;
                 }
 
-                static::handleError($scope);
+                $span = static::handleError($scope);
+                SamplerSpanFilter::filter($span, true);
             },
         );
         // phpcs:enable
@@ -109,9 +111,9 @@ class MerchantPortalApplicationInstrumentation
     /**
      * @param \OpenTelemetry\Context\ContextStorageScopeInterface $scope
      *
-     * @return \OpenTelemetry\API\Trace\SpanInterface
+     * @return \OpenTelemetry\SDK\Trace\ReadableSpanInterface
      */
-    protected static function handleError(ContextStorageScopeInterface $scope): SpanInterface
+    protected static function handleError(ContextStorageScopeInterface $scope): ReadableSpanInterface
     {
         $error = error_get_last();
         $exception = null;
@@ -133,8 +135,7 @@ class MerchantPortalApplicationInstrumentation
         $span->setAttribute(static::ERROR_CODE, $exception !== null ? $exception->getCode() : '');
         $span->setStatus($exception !== null ? StatusCode::STATUS_ERROR : StatusCode::STATUS_OK);
 
-        $span->end();
-
+        /** @var \OpenTelemetry\SDK\Trace\ReadableSpanInterface $span */
         return $span;
     }
 
